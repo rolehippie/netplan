b9834d0ece00e8bf1f38ef47d83a5e09cbec8bf3
Thomas Boerger <thomas@webhippie.de>
2020-10-26 13:08:34 +0100

    Test if netplan.io gets installed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,5 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_is_installed(host):
-    pkg = host.package("nplan")
+    pkg = host.package("netplan.io")
     assert pkg.is_installed
